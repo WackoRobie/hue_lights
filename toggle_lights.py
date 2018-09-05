09459c4a9453e63ad18cf9d2b3e6aa4da9f79ec5
Steven Robinson <srobn@amazon.com>
2018-09-05 14:07:10 -0700

    Groups toggle working
    Added gitignore. Also added group toggle funtionality and logging.

--- toggle_lights.py
+++ toggle_lights.py
@@ -1,19 +1,69 @@
 #! /usr/env/python
 
 import requests
-import json
+import sys
+import logging
 
 IP = '192.168.188.22'
 USER = '2TVMBPdD12TsazTaC474o1mEK5KZODZ8FEsAs199'
-URL = f"http://{IP}/api/2TVMBPdD12TsazTaC474o1mEK5KZODZ8FEsAs199/groups/"
+BASE_URL = f"http://{IP}/api/{USER}/"
+format = '%(asctime)s : %(levelname)s : %(name)s.%(funcName)s : %(message)s'
+logger = logging.getLogger()
+formatter = logging.Formatter(format)
+logger.setLevel(logging.DEBUG)
 
-selection = 'groups'
+file_handler = logging.FileHandler(filename='toggle.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
 
-r = requests.get(f"http://{IP}/api/{USER}/{selection}/")
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
 
-response = json.loads(r.text)
+logger.addHandler(file_handler)
 
-for group in response.keys():
-    name = response[group]['name']
-    state = True if response[group]['state']['all_on'] == 'true' else False
-    print(f'Group: {name}:{state}')
+
+def toggleLightByNumber(light):
+    # Takes in light as integer to have state changed
+    LIGHT_URL = f'lights/{light}/'
+    get = requests.get(BASE_URL + LIGHT_URL)
+    setstate = not get.json()['state']['on']
+    setstate = str(setstate).lower()
+    mydata = f'{{"on": {setstate}}}'
+    logging.info('Changing Light State')
+    put = requests.put(BASE_URL+LIGHT_URL+'state/', data=mydata)
+    logging.debug(put.json())
+
+
+def toggleGroupByNumber(group):
+    # Takes in a group as an integer to have state changed
+    GROUP_URL = f'groups/{group}/'
+    get = requests.get(BASE_URL + GROUP_URL)
+    setstate = not get.json()['action']['on']
+    setstate = str(setstate).lower()
+    mydata = f'{{"on": {setstate}}}'
+    logging.info("Changing Group State")
+    put = requests.put(BASE_URL + GROUP_URL + 'action/', data=mydata)
+    logging.debug(put.json())
+    toggleLightByNumber(1)
+    counter = 5
+    while True:
+        counter -= 1
+        get = requests.get(BASE_URL + GROUP_URL)
+        all_on = get.json()['state']['all_on']
+        any_on = get.json()['state']['any_on']
+        if any_on == all_on:
+            break
+        else:
+            logging.info("Changing Group State")
+            put = requests.put(BASE_URL + GROUP_URL + 'action/', data=mydata)
+            logging.debug(put.json())
+            # toggleLightByNumber(1)
+        if counter <= 0:
+            logging.warning("Failed to flip state of all lights")
+            sys.exit("FAILED TO FLIP STATE OF ALL LIGHTS!")
+
+
+if __name__ == '__main__':
+    logger.addHandler(stream_handler)
+    toggleGroupByNumber(1)
